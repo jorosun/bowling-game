23063ee80cc1012587da37d2501064b3201526f9
Jennifer Orosun <orosunj@gmail.com>
2023-06-05 01:10:57 +0100

    setup for first test

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-export const createRoll = () => {};
+export const createRoll = () => {
+  // get the user to input the number of pins knocked down
+  // let roll = ("please input number of pins knocked down");
+  let roll = 10;
+  return roll;
+};
+
+export const createFrame = () => {
+  let spare = false;
+  let strike = false;
+  let usedRoll2 = false;
+
+  let roll1 = createRoll();
+
+  if (roll1 === 10) {
+    strike = true;
+  } else {
+    let roll2 = createRoll();
+    usedRoll2 = true;
+  }
+  console.log(strike);
+  console.log(roll1);
+  console.log(usedRoll2);
+};
--- index.test.js
+++ index.test.js
@@ -1,7 +1,16 @@
-import { createRoll } from "./index.js";
-
+import { createRoll, createFrame } from "./index.js";
+/*
 describe("createRoll", () => {
-  test("returns number of pins knocked down", () => {
-    expect(createRoll()).toBe("1");
+  test("returns number of pins knocked down in the roll", () => {
+    expect(createRoll()).toBe(1);
+  });
+  test("returns number of pins knocked down in the roll", () => {
+    expect(createRoll()).toBe(10);
+  });
+});
+*/
+describe("createFrame", () => {
+  test("returns number of pins knocked down in the frame ", () => {
+    expect(createFrame()).toBe(10);
   });
 });
